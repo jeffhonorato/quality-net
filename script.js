29b796823f86a47dc3b8855d97cb41a3665eb432
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2021-12-06 11:05:08 -0300

    Adicionando MENU MOBILE - JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu() {
+    const nav = document.querySelector(".nav-item")
+    nav.classList.toggle('ativo')
+}
+
+
+btnMobile.addEventListener('click', toggleMenu)
