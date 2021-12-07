3a6baa04dae51a6c6dd964aed82db35b9cac44f0
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2021-12-07 08:49:06 -0300

    Responsividade e Menu Mobile

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu() {
     const nav = document.querySelector(".nav-item")
-    nav.classList.toggle('ativo')
+    nav.classList.toggle('ativo');
 }
-
 
 btnMobile.addEventListener('click', toggleMenu)
