0a2e2c5f84b5745860b7b81fe289b27a8ffbc857
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-03-29 11:23:43 -0300

    Ajustes

--- swiper.js
+++ swiper.js
@@ -8,7 +8,28 @@ var swiper = new Swiper(".mySwiper", {
       stretch: 0,
       depth: 100,
       modifier: 1,
-      slideShadows: true,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".viaRadio", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
     },
     pagination: {
       el: ".swiper-pagination",
